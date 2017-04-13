fc5756591e0073fdfae66195300035e245479622
Petar Efnushev <petar.efnushev@keitaro.com>
2017-04-13 11:19:23 +0200

    Add custom actions for user_create, user_update

---
+++ ckanext/password/actions.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -
+import logging
+import ckan.logic as l
+import ckanext.password.helpers as _h
+from ckan.common import _
+
+try:
+    # CKAN 2.7 and later
+    from ckan.common import config
+except ImportError:
+    # CKAN 2.6 and earlier
+    from pylons import config
+
+log = logging.getLogger(__name__)
+
+
+def user_create(context, data_dict):
+    password = data_dict.get('password1', '')
+    if 'password' in data_dict:
+        password = data_dict['password']
+
+    valid = _h.validate_password(password)
+    if not valid:
+        msg = config.get('ckanext.password.invalid_password_message', None)
+        if msg is None:
+            msg = _('Password must consists of 8-15 alphanumeric characters, contain at least one uppercase \
+                    letter and at least one of the following special characters: !, @, #, $, %, ^, &, *.')
+        raise l.ValidationError({'password': [msg]})
+    return l.action.create.user_create(context, data_dict)
+
+
+def user_update(context, data_dict):
+    password = data_dict.get('password1', '')
+    if 'password' in data_dict:
+        password = data_dict['password']
+
+    valid = _h.validate_password(password)
+    if not valid:
+        msg = config.get('ckanext.password.invalid_password_message', None)
+        if msg is None:
+            msg = _('Password must consists of 8-15 alphanumeric characters, contain at least one uppercase \
+                    letter and at least one of the following special characters: !, @, #, $, %, ^, &, *.')
+        raise l.ValidationError({'password': [msg]})
+    return l.action.update.user_update(context, data_dict)
