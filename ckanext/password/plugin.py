4f718262e7a985c5f1e0f2061d2830e6af1d64cf
Petar Efnushev <petar.efnushev@keitaro.com>
2017-04-13 11:21:59 +0200

    Implement IActions interface

--- ckanext/password/plugin.py
+++ ckanext/password/plugin.py
@@ -1,9 +1,11 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
+import ckanext.password.actions as a
 
 
 class PasswordPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IActions)
 
     # IConfigurer
 
@@ -11,3 +13,10 @@ class PasswordPlugin(plugins.SingletonPlugin):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'password')
+
+    ## IActions
+    def get_actions(self):
+        return {
+            'user_create': a.user_create,
+            'user_update': a.user_update
+        }
