b8748c55b156bbd057866675f873323b0a6928de
Petar Efnushev <petar.efnushev@keitaro.com>
2017-07-14 16:40:58 +0200

    Override `perform_reset` action

--- ckanext/password/plugin.py
+++ ckanext/password/plugin.py
@@ -2,10 +2,13 @@ import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 import ckanext.password.actions as a
 
+from ckanext.password.controller import CTRL
+
 
 class PasswordPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
     plugins.implements(plugins.IActions)
+    plugins.implements(plugins.IRoutes, inherit=True)
 
     # IConfigurer
 
@@ -20,3 +23,9 @@ class PasswordPlugin(plugins.SingletonPlugin):
             'user_create': a.user_create,
             'user_update': a.user_update
         }
+
+    # IRoutes
+
+    def before_map(self, map):
+        map.connect('/user/reset/{id:.*}', controller=CTRL, action='perform_reset')
+        return map
